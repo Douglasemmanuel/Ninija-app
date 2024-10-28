b0497857f2908471a52d6196a25f37b0a921b8fe
DOUGLAS EMMANUEL <emmanueldouglas2121@gmail.com>
2024-10-28 12:40:28 +0000

    learned how to update a state

--- lib/main.dart
+++ lib/main.dart
@@ -4,94 +4,111 @@ import 'package:flutter/material.dart';
 //   runApp(const MyApp());
 // }
 void main() => runApp(MaterialApp(
-  home:NinjaCard(),
-));
-class NinjaCard extends StatelessWidget {
-  // const MyWidget({super.key});
+      home: NinjaCard(),
+    ));
 
+class NinjaCard extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Ninja ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body:Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-           Center(
-             child:CircleAvatar(
-              backgroundImage: AssetImage('assets/redbag1.png'),
-              radius: 80.0,
-            ),
-           ),
-           Divider(
-            height: 90.0,
-            color: Colors.grey[800],
-           ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Douglas',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '8',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children:<Widget>[
-                Icon(
-                  Icons.email,
-                color: Colors.grey[400],
-                ),
-                 SizedBox(width: 10.0),
-                Text(
-                  'emmanueldouglas@thenetninja.co.uk',
-                  style: TextStyle(
-                    color:Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-                
-              ],
-              ),
-          ],
-        ),
-        )
-    );
-  }
+  State<NinjaCard> createState() => _NinjaCardState();
 }
 
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjalevel = 0;
+  // const MyWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text(
+            'Ninja ID Card',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjalevel += 1;   //to update a state in flutter
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/redbag1.png'),
+                  radius: 80.0,
+                ),
+              ),
+              Divider(
+                height: 90.0,
+                color: Colors.grey[800],
+              ),
+              Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Douglas',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '$ninjalevel',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'emmanueldouglas@thenetninja.co.uk',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
